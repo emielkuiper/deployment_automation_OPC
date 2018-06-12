9f7397a028caa641147eb0a49b2ad2aad1a1ac95
Emiel Kuiper <emielkuiper@gmail.com>
2018-06-12 15:43:13 +0200

    ACLs easier to read

--- security.tf
+++ security.tf
@@ -5,19 +5,9 @@
 ### The opc_compute_security_ip_list resource creates and manages
 ### a security IP list in an Oracle Cloud Infrastructure Compute Classic identity domain.
 
-resource "opc_compute_security_ip_list" "sec_ip_list_ssh_access" {
-  name       = "sec_ip_list_ssh_access"
-  ip_entries = ["84.241.194.88"]
-}
-
-resource "opc_compute_security_ip_list" "sec_ip_clients" {
-  name       = "remote-management-clients"
-  ip_entries = ["86.92.159.160", "198.184.231.254"]
-}
-
-resource "opc_compute_security_ip_list" "IP_Emiel_huis" {
-  name       = "IP_Emiel_huis"
-  ip_entries = ["86.92.159.160"]
+resource "opc_compute_security_ip_list" "sec_ip_list_EXTERNAL_MGMT_IPS" {
+  name       = "sec_ip_list_external_mgmt_ips"
+  ip_entries = ["86.92.159.160", "198.184.231.254", "84.241.194.88"]
 }
 
 ###
@@ -27,8 +17,8 @@ resource "opc_compute_security_ip_list" "IP_Emiel_huis" {
 ## The opc_compute_security_list resource creates and manages
 ## a security list in an Oracle Cloud Infrastructure Compute Classic identity domain.
 
-resource "opc_compute_security_list" "sec_list_INCOMING_TRAFFIC" {
-  name                 = "sec_list_INCOMING_TRAFFIC"
+resource "opc_compute_security_list" "sec_list_ALL_INBOUND_TRAFFIC" {
+  name                 = "sec_list_ALL_INBOUND_TRAFFIC"
   policy               = "permit"
   outbound_cidr_policy = "deny"
 }
@@ -37,24 +27,37 @@ resource "opc_compute_security_list" "sec_list_INCOMING_TRAFFIC" {
 ### Application definitions
 ###
 
-resource "opc_compute_security_application" "ssh" {
+resource "opc_compute_security_application" "SSH" {
   name     = "ssh"
   protocol = "tcp"
   dport    = "22"
+}
+
+resource "opc_compute_security_application" "RDP" {
+  name     = "rdp"
+  protocol = "tcp"
+  dport    = "3389"
 }
 
 ###
 ### SEC_RULE: Actual firewall policy
 ###
 
-resource "opc_compute_sec_rule" "sec_rule_INCOMING_TRAFFIC" {
-  name             = "test_rule_INCOMING_TRAFFIC"
-  source_list      = "seciplist:${opc_compute_security_ip_list.sec_ip_list_ssh_access.name}"
-  destination_list = "seclist:${opc_compute_security_list.sec_list_INCOMING_TRAFFIC.name}"
+resource "opc_compute_sec_rule" "sec_rule_INBOUND_SSH" {
+  name             = "test_rule_INBOUND_SSH"
+  source_list      = "seciplist:${opc_compute_security_ip_list.sec_ip_list_EXTERNAL_MGMT_IPS.name}"
+  destination_list = "seclist:${opc_compute_security_list.sec_list_ALL_INBOUND_TRAFFIC.name}"
   action           = "permit"
-  application      = "${opc_compute_security_application.ssh.name}"
+  application      = "${opc_compute_security_application.RDP.name}"
 }
 
+resource "opc_compute_sec_rule" "sec_rule_INBOUND_RDP" {
+  name             = "test_rule_INBOUND_RDP"
+  source_list      = "seciplist:${opc_compute_security_ip_list.sec_ip_list_EXTERNAL_MGMT_IPS.name}"
+  destination_list = "seclist:${opc_compute_security_list.sec_list_ALL_INBOUND_TRAFFIC.name}"
+  action           = "permit"
+  application      = "${opc_compute_security_application.RDP.name}"
+}
 
 ###
 ### Security associations
@@ -66,17 +69,17 @@ resource "opc_compute_sec_rule" "sec_rule_INCOMING_TRAFFIC" {
 resource "opc_compute_security_association" "test_instance__sec_list__instance_cicd" {
   name    = "SA_instance_cicd"
   vcable  = "${opc_compute_instance.instance_cicd.vcable}"
-  seclist = "${opc_compute_security_list.sec_list_INCOMING_TRAFFIC.name}"
+  seclist = "${opc_compute_security_list.sec_list_ALL_INBOUND_TRAFFIC.name}"
 }
 
 resource "opc_compute_security_association" "test_instance__sec_list__instance_siebel" {
   name    = "SA_instance_siebel"
   vcable  = "${opc_compute_instance.instance_siebel.vcable}"
-  seclist = "${opc_compute_security_list.sec_list_INCOMING_TRAFFIC.name}"
+  seclist = "${opc_compute_security_list.sec_list_ALL_INBOUND_TRAFFIC.name}"
 }
 
 resource "opc_compute_security_association" "test_instance__sec_list__instance_siebel_configurator" {
   name    = "SA_instance_siebel_configurator"
   vcable  = "${opc_compute_instance.instance_siebel_configurator.vcable}"
-  seclist = "${opc_compute_security_list.sec_list_INCOMING_TRAFFIC.name}"
+  seclist = "${opc_compute_security_list.sec_list_ALL_INBOUND_TRAFFIC.name}"
 }
